f28fe34c1d25169a50bfdf0f07a243448da1edb4
Udit <uditde.com@gmail.com>
2023-04-18 23:56:37 +0530

    eslint config done

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"@react-native-community",
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		semi: ["error", "never"],
+		"comma-dangle": [2, "never"],
+		"react/jsx-filename-extention": [1, { extensions: [".js", ".jsx"] }],
+		"no-use-before-define": [
+			"error",
+			{ function: true, classes: true, variables: false },
+		],
+	},
+};
